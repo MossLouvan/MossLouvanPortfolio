d39bf2261f78bae39f4692417156c5e94cfce07c
MossLouvan <134662155+MossLouvan@users.noreply.github.com>
2026-02-20 13:26:02 -0600

    Removed public from the path and now it works

--- app/api/achievements/images.ts
+++ app/api/achievements/images.ts
@@ -1,6 +1,6 @@
 export const images = [
-  "/public/achievements/Me presenting at the Houston Space Center.jpg",
-  "/public/achievements/Me presenting at the Johnston Space Center to NASA employees.jpg",
-  "public/achievements/Me presenting at the Technology Assosiation of Iowa.jpg",
-  "public/achievements/My meeting with the CEO of Principal Financial Group Dan Houston.jpg"
+  "/achievements/Me presenting at the Houston Space Center.jpg",
+  "/achievements/Me presenting at the Johnston Space Center to NASA employees.jpg",
+  "/achievements/Me presenting at the Technology Assosiation of Iowa.jpg",
+  "/achievements/My meeting with the CEO of Principal Financial Group Dan Houston.jpg",
 ];
