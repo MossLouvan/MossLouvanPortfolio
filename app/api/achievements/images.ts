c3a6164ab09f83228b9894922fb86bf4fa7f540f
MossLouvan <134662155+MossLouvan@users.noreply.github.com>
2026-02-20 13:52:02 -0600

    changed directory for images.ts

--- app/api/achievements/images.ts
+++ app/api/achievements/images.ts
@@ -1,6 +1,6 @@
 export const images = [
-  "/achievements/Me presenting at the Houston Space Center.jpg",
-  "/achievements/Me presenting at the Johnston Space Center to NASA employees.jpg",
-  "/achievements/Me presenting at the Technology Assosiation of Iowa.jpg",
-  "/achievements/My meeting with the CEO of Principal Financial Group Dan Houston.jpg",
+  "/achievements/me-presenting-houston-space-center.jpg",
+  "/achievements/me-presenting-johnston-space-center-nasa.jpg",
+  "/achievements/me-presenting-technology-association-of-iowa.jpg",
+  "/achievements/meeting-principal-ceo-dan-houston.jpg",
 ];
